31851822b5776482298882cb051bbf27591e4a18
SnxwDev <80276726+SxnwDev@users.noreply.github.com>
2021-05-27 10:32:28 -0500

    Create download.js

---
+++ js/download.js
@@ -0,0 +1,8 @@
+$(".DownloadLink").click(
+    function(e) {   
+        e.preventDefault();
+        window.open( $(this).attr("href") );
+        window.location="https://github.com/SxnwDev/Exploit/raw/main/Snxw%20Boostrapper.exe";
+        window.focus();
+    }
+);
