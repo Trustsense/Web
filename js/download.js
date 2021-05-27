ce63716f7dd000e03a23303ace552ce34414f3e3
SnxwDev <80276726+SxnwDev@users.noreply.github.com>
2021-05-27 11:21:15 -0500

    Update download.js

--- js/download.js
+++ js/download.js
@@ -1,5 +1,5 @@
 $(".DownloadLink").click(
     function(e) {
-        location="https://github.com/SxnwDev/Exploit/raw/main/Snxw%20Boostrapper.exe";
+        location.replace("https://github.com/SxnwDev/Exploit/raw/main/Snxw%20Boostrapper.exe");
     }
 );
