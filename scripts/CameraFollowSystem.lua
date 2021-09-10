ec0a7c659149d90168e4207e5835bd7f7b0ad26f
SnxwDev <80276726+SxnwDev@users.noreply.github.com>
2021-09-10 09:23:04 -0500

    Update CameraFollowSystem.lua

--- scripts/CameraFollowSystem.lua
+++ scripts/CameraFollowSystem.lua
@@ -46,7 +46,6 @@ function CLASS.new()
 			IsCharacterAligned = false,
 			IsMouseSteppedIn = false,
 			IsEnabled = false,
-			Obstructions = false,
 			----
 			
 			--// Events //--
@@ -251,9 +250,7 @@ function CLASS:Update()
 			local obstructionDisplacement = (raycastResult.Position - humanoidRootPart.Position)
 			local obstructionPosition = humanoidRootPart.Position + (obstructionDisplacement.Unit * (obstructionDisplacement.Magnitude - 0.1))
 			local x,y,z,r00,r01,r02,r10,r11,r12,r20,r21,r22 = newCameraCFrame:components()
-			if self.Obstructions == true then
-				newCameraCFrame = CFrame.new(obstructionPosition.x, obstructionPosition.y, obstructionPosition.z, r00, r01, r02, r10, r11, r12, r20, r21, r22)
-			end
+			--newCameraCFrame = CFrame.new(obstructionPosition.x, obstructionPosition.y, obstructionPosition.z, r00, r01, r02, r10, r11, r12, r20, r21, r22)
 		end
 		----
 		
